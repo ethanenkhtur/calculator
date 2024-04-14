8565e466faa469bb2a6488d12f57b296abac37cc
ethanenkhtur <98515458+ethanenkhtur@users.noreply.github.com>
2024-04-14 17:06:39 -0500

    Operators function

--- script.js
+++ script.js
@@ -1,32 +1,75 @@
-let first_number, operator, second_number;
+let first_number = 0,
+    operator,
+    second_number;
 
 const calculator = document.querySelector(".calculator");
 const display = document.querySelector(".display");
 display.textContent = "";
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const operators = ["+", "-", "*", "/"];
 
 const buttons = calculator.querySelectorAll(".button");
 buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
-        clicked_number = event.target.textContent;
-        display.textContent = clicked_number;
+        let ready_to_be_operated = false;
+
+        clicked_button = event.target.textContent;
+
+        display.textContent += clicked_button;
+
+        if (operators.includes(clicked_button)) {
+            first_number = display.textContent.slice(
+                0,
+                display.textContent.length - 1
+            );
+            operator = clicked_button;
+            console.log(operator);
+            display.textContent = "";
+            console.log(first_number);
+        }
+
+        if (clicked_button === "=") {
+            second_number = display.textContent;
+            display.textContent = "";
+            display.textContent = operate(
+                first_number,
+                operator,
+                second_number
+            );
+        }
+
+        if (clicked_button === "C") clear();
+
+        // if (non_displaying_operators.includes(clicked_button)) {
+        //     let non_displaying_operator = clicked_button;
+        // } else if (numbers.includes(clicked_button)) {
+        //     display_section += clicked_button;
+        // }
     });
 });
 
+function clear() {
+    display.textContent = "";
+    first_number = null;
+    second_number = null;
+    operator = null;
+}
+
 function operate(num1, operator, num2) {
-    const result = 0;
+    let result = 0;
 
     switch (operator) {
         case "+":
-            result = add(num1, num2);
+            result = add(parseInt(num1), parseInt(num2));
             break;
         case "-":
-            result = substract(num1, num2);
+            result = subtract(parseInt(num1), parseInt(num2));
             break;
         case "*":
-            result = multiply(num1, num2);
+            result = multiply(parseInt(num1), parseInt(num2));
             break;
         case "/":
-            result = divide(num1, num2);
+            result = divide(parseInt(num1), parseInt(num2));
             break;
     }
 
@@ -37,7 +80,7 @@ function add(num1, num2) {
     return num1 + num2;
 }
 
-function substract(num1, num2) {
+function subtract(num1, num2) {
     return num1 - num2;
 }
 

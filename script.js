f753459764d2fe0c181783e4e586f61b070ec863
ethanenkhtur <98515458+ethanenkhtur@users.noreply.github.com>
2024-04-14 14:56:20 -0500

    added operate() function

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
+let first_number, operator, second_number;
+
+function operate(num1, operator, num2) {
+    const result = 0;
+
+    switch (operator) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = substract(num1, num2);
+            break;
+        case "*":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+    }
+
+    return result;
+}
+
 function add(num1, num2) {
-  return num1 + num2;
+    return num1 + num2;
 }
 
 function substract(num1, num2) {
-  return num1 - num2;
+    return num1 - num2;
 }
 
 function multiply(num1, num2) {
-  return num1 * num2;
+    return num1 * num2;
 }
 
 function divide(num1, num2) {
-  return num1 / num2;
+    return num1 / num2;
 }

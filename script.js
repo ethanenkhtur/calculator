c23e8575b7560739bf958fbcab5eb3d0d9af4823
ethanenkhtur <98515458+ethanenkhtur@users.noreply.github.com>
2024-04-18 11:48:33 -0500

    Improvement
    Consequtive operations can be operated ONLY after pressing EQUAL sign

--- script.js
+++ script.js
@@ -18,27 +18,48 @@ buttons.forEach((button) => {
 });
 
 function decideAction(clickedButton) {
-    console.log("ITERATION STARTS");
+    // console.log("ITERATION STARTS");
 
     if (numbers.includes(clickedButton)) {
         display.textContent += clickedButton;
     }
 
+    if (operators.includes(clickedButton)) {
+        if (!one_operation) {
+            first_number = display.textContent;
+        }
+
+        operator = clickedButton;
+        clearDisplay();
+    }
+
+    if (clickedButton === "=") {
+        second_number = display.textContent;
+        clearDisplay();
+
+        display.textContent = operate(first_number, operator, second_number);
+        first_number = operate(first_number, operator, second_number);
+        console.log("FIRST NUMBER: " + first_number);
+        one_operation = true;
+    }
+
+    if (clickedButton === "C") clear();
+
     // ------------------------------------------------------
 
     // if (numbers.includes(clickedButton)) {
     //     if (operator === "") {
-            // if (!one_operation) {
-            //     first_number += clickedButton;
-            //     console.log(
-            //         first_number + " => FIRST NUMBER  WITH ONEOPERATION = false"
-            //     );
-            // } else {
-            //     first_number = operate(first_number, operator, second_number);
-            //     console.log(
-            //         first_number + " => FIRST NUMBRER WITH ONEOPERATION = true"
-            //     );
-            // }
+    // if (!one_operation) {
+    //     first_number += clickedButton;
+    //     console.log(
+    //         first_number + " => FIRST NUMBER  WITH ONEOPERATION = false"
+    //     );
+    // } else {
+    //     first_number = operate(first_number, operator, second_number);
+    //     console.log(
+    //         first_number + " => FIRST NUMBRER WITH ONEOPERATION = true"
+    //     );
+    // }
     //         if (!one_operation) first_number += clickedButton;
     //     } else {
     //         second_number += clickedButton;
@@ -46,86 +67,85 @@ function decideAction(clickedButton) {
     //         one_operation = true;
     //     }
 
-        // if (one_operation) {
-        //     first_number = operate(
-        //         display.textContent,
-        //         operator,
-        //         second_number
-        //     );
-        // }
-    }
-
-    // if (operators.includes(clickedButton)) {
-    //     operator = clickedButton;
-    //     console.log(operator + " => THIS IS CURRENT OPERATOR");
-    //     console.log(
-    //         "Currently number 1: " +
-    //             first_number +
-    //             "\nNumber 2: " +
-    //             second_number
+    // if (one_operation) {
+    //     first_number = operate(
+    //         display.textContent,
+    //         operator,
+    //         second_number
     //     );
-    //     if (one_operation) {
-    //         first_number = operate(first_number, operator, second_number);
-    //     }
-
-    //     clearDisplay();
-    // }
-
-    // if (clickedButton === "=") {
-    //     display.textContent = operate(first_number, operator, second_number);
-    //     console.log(
-    //         "RESULT: " + operate(first_number, operator, second_number)
-    //     );
-    //     one_operation = false;
-    // }
-
-    // if (clickedButton === "C") clear();
-
-    // console.log("ITERATION ENDS");
-
-    //---------------------------------------------------------
-
-
-    // if (operators.includes(clickedButton)) {
-    //     if (!one_operation) {
-    //         first_number = display.textContent;
-    //         operator = clickedButton;
-    //         one_operation = true;
-
-    //         clearDisplay();
-    //     } else {
-    //         second_number = display.textContent;
-    //         clearDisplay();
-    //         first_number = operate(first_number, operator, second_number);
-    //         display.textContent = first_number;
-    //     }
-    //     console.log(one_operation);
-    // }
-
-    // if (clickedButton === "=") {
-    //     second_number = display.textContent;
-    //     clearDisplay();
-    //     display.textContent = operate(first_number, operator, second_number);
-    //     one_operation = true;
-    // }
-
-    // if (clickedButton === "C") clear();
-
-    // ------------------------------------------
-
-    // if (operators.includes(clickedButton)) {
-    //     first_number = display.textContent;
-    //     operator = clickedButton;
-
-    //     clearDisplay();
-    // }
-
-    // if (clickedButton === "=") {
-    //     second_number = display.textContent;
-    //     clearDisplay();
-    //     display.textContent = operate(first_number, operator, second_number);
     // }
 }
+
+// if (operators.includes(clickedButton)) {
+//     operator = clickedButton;
+//     console.log(operator + " => THIS IS CURRENT OPERATOR");
+//     console.log(
+//         "Currently number 1: " +
+//             first_number +
+//             "\nNumber 2: " +
+//             second_number
+//     );
+//     if (one_operation) {
+//         first_number = operate(first_number, operator, second_number);
+//     }
+
+//     clearDisplay();
+// }
+
+// if (clickedButton === "=") {
+//     display.textContent = operate(first_number, operator, second_number);
+//     console.log(
+//         "RESULT: " + operate(first_number, operator, second_number)
+//     );
+//     one_operation = false;
+// }
+
+// if (clickedButton === "C") clear();
+
+// console.log("ITERATION ENDS");
+
+//---------------------------------------------------------
+
+// if (operators.includes(clickedButton)) {
+//     if (!one_operation) {
+//         first_number = display.textContent;
+//         operator = clickedButton;
+//         one_operation = true;
+
+//         clearDisplay();
+//     } else {
+//         second_number = display.textContent;
+//         clearDisplay();
+//         first_number = operate(first_number, operator, second_number);
+//         display.textContent = first_number;
+//     }
+//     console.log(one_operation);
+// }
+
+// if (clickedButton === "=") {
+//     second_number = display.textContent;
+//     clearDisplay();
+//     display.textContent = operate(first_number, operator, second_number);
+//     one_operation = true;
+// }
+
+// if (clickedButton === "C") clear();
+
+// ------------------------------------------
+
+// if (operators.includes(clickedButton)) {
+//     first_number = display.textContent;
+//     operator = clickedButton;
+
+//     clearDisplay();
+// }
+
+// if (clickedButton === "=") {
+//     second_number = display.textContent;
+//     clearDisplay();
+//     display.textContent = operate(first_number, operator, second_number);
+// }
+// }
 
 function clearDisplay() {
     display.textContent = "";
